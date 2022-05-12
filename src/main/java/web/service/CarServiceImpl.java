2018cfd78098f0894777f085881eb1c0f658615b
drkpo <drkpolice666@@gmail.com>
2022-05-12 05:13:16 +0300

    Fix 12

--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -11,16 +11,19 @@ public class CarServiceImpl implements CarService {
 
     List<Car> carsList = new ArrayList<>();
 
-    @Override
-    public List<Car> getCarsList(int count) {
-        if (count != 0 && count <= carsList.size() && count >= 0) {
-            return carsList.stream().limit(count).collect(Collectors.toList());
-        }
+    {
         carsList.add(new Car("BMW", "white", 2018));
         carsList.add(new Car("Reno", "green", 2015));
         carsList.add(new Car("Citroen", "blue", 2022));
         carsList.add(new Car("Lada", "black", 2014));
         carsList.add(new Car("Audi", "red", 2019));
-        return carsList.stream().limit(count).collect(Collectors.toList());
+    }
+
+    @Override
+    public List<Car> getCarsList(int count) {
+        if (count != 0 && count >= 0 && count <= carsList.size()) {
+            return carsList.stream().limit(count).collect(Collectors.toList());
+        }
+        return carsList;
     }
 }
